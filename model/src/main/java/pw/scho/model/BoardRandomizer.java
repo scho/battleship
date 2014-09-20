73fd4dbd487ce9b94b2f3b8f52b230055dd9e797
Georg Meyer <georgmeyer83@googlemail.com>
2014-09-20 15:08:12 +0200

    Add board randomizer

--- model/src/main/java/pw/scho/model/BoardRandomizer.java
+++ model/src/main/java/pw/scho/model/BoardRandomizer.java
@@ -1,8 +1,42 @@
 package pw.scho.model;
 
+import java.util.Random;
+
 public class BoardRandomizer {
 
-    public void randomize(Board board) {
+    private final Random random = new Random();
+    private final Board board = new Board();
 
+    public Board randomize(int[] shipSizes) {
+
+        for(int size : shipSizes){
+            placeShip(size);
+        }
+
+        return board;
+    }
+
+    private void placeShip(int size) {
+        while(true){
+            Ship ship = createRandomShip(size);
+
+            if(board.shipIsPlaceable(ship)){
+                board.placeShip(ship);
+                break;
+            }
+        }
+    }
+
+    private Ship createRandomShip(int size) {
+        int x = random.nextInt(Board.WIDTH - 1);
+        int y = random.nextInt(Board.HEIGHT - 1);
+
+        Position position = new Position(x, y);
+
+        if(random.nextBoolean()){
+            return Ship.createHorizontal(position, size);
+        } else {
+            return Ship.createVertical(position, size);
+        }
     }
 }
---
+++ model/src/test/java/pw/scho/model/BoardRandomizerTest.java
@@ -0,0 +1,28 @@
+package pw.scho.model;
+
+import org.junit.Test;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
+
+public class BoardRandomizerTest {
+
+    private BoardRandomizer boardRandomizer = new BoardRandomizer();
+
+    @Test
+    public void testAddsCorrectNumberOfShips(){
+        Board board = boardRandomizer.randomize(new int[]{2, 3, 4});
+
+        assertThat(board.getShips().size(), is(3));
+    }
+
+    @Test
+    public void testAddsCorrectSize(){
+        Board board = boardRandomizer.randomize(new int[]{2, 3, 4});
+
+        assertThat(board.getShips().get(0).size(), is(2));
+        assertThat(board.getShips().get(1).size(), is(3));
+        assertThat(board.getShips().get(2).size(), is(4));
+    }
+
+}
