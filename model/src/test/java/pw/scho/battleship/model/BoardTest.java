0a4a4c995a90d0811c7c723c2b1a872651d14766
Georg Meyer <georgmeyer83@googlemail.com>
2014-09-21 10:36:58 +0200

    Consolidate board tests into one class

--- model/src/test/java/pw/scho/battleship/model/BoardTest.java
+++ model/src/test/java/pw/scho/battleship/model/BoardTest.java
@@ -1,13 +1,21 @@
 package pw.scho.battleship.model;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.is;
 
-public class BoardShipPlacingTest {
+public class BoardTest {
 
-    private Board board = new Board();
+    private Board board;
+
+    @Before
+    public void createBoard(){
+        board  = new Board();
+    }
+
+    // Placing of ships
 
     @Test
     public void testShipIsPlaceableForEmptyBoard() {
@@ -71,5 +79,51 @@ public class BoardShipPlacingTest {
 
         board.placeShip(ship);
         board.placeShip(otherShip);
+    }
+
+    // Shooting at ships
+
+    @Test
+    public void testIsShootableAtIsFalseIfAlreadyShotAt() {
+        board.shootAt(new Position(0, 0));
+
+        assertThat(board.isShootableAt(new Position(0, 0)), is(false));
+    }
+
+    @Test
+    public void testIsShootableAtIsTrueIfNotShotAt() {
+        assertThat(board.isShootableAt(new Position(0, 0)), is(true));
+    }
+
+    @Test(expected = GameException.class)
+    public void testIllegalVerticalPlacementThrowsException() {
+        board.shootAt(new Position(0, 0));
+        board.shootAt(new Position(0, 0));
+    }
+
+    @Test
+    public void testAllShipsSunkIsTrueIfAllShipsWereFullyHit() {
+        Ship ship = Ship.createHorizontal(new Position(0, 0), 2);
+
+        board.placeShip(ship);
+        board.shootAt(new Position(0, 0));
+        board.shootAt(new Position(1, 0));
+
+        assertThat(board.allShipsSunk(), is(true));
+    }
+
+    @Test
+    public void testAllShipsSunkIsFalseIfSomeShipsPartiallyRemain() {
+        Ship ship = Ship.createHorizontal(new Position(0, 0), 2);
+        Ship otherShip = Ship.createVertical(new Position(3, 3), 3);
+
+        board.placeShip(ship);
+        board.placeShip(otherShip);
+
+        board.shootAt(new Position(0, 0));
+        board.shootAt(new Position(1, 0));
+        board.shootAt(new Position(3, 3));
+
+        assertThat(board.allShipsSunk(), is(false));
     }
 }
